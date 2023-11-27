86a9cf0603ccf2e317bbea8b0b97e8c6bb47611a
anton <ant.arhipov@gmail.com>
2023-11-27 15:50:05 +0200

    initial commit

---
+++ multiplatform/settings.gradle.kts
@@ -0,0 +1,22 @@
+rootProject.name = "multiplatform"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://oss.sonatype.org/content/repositories/orgjetbrainskotlinx-2948")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+include(":composeApp")
